292a1b9072517cbedaef9dd37bfa45c516fa854f
rasmushr97 <rasmushr97@gmail.com>
2026-06-24 23:29:35 +0200

    Support percent comment Molten cells

--- lua/config/notebook.lua
+++ lua/config/notebook.lua
@@ -18,7 +18,59 @@ function M.define_visual_cell()
   define_cell(vim.fn.getpos("'<")[2], vim.fn.getpos("'>")[2])
 end
 
+local function is_percent_marker(line)
+  return line:match("^%s*#%s*%%%%") ~= nil
+end
+
+function M.define_percent_cell()
+  local current = vim.api.nvim_win_get_cursor(0)[1]
+  local last = vim.fn.line("$")
+  local marker = nil
+
+  for line = current, 1, -1 do
+    if is_percent_marker(vim.fn.getline(line)) then
+      marker = line
+      break
+    end
+  end
+
+  if not marker then
+    return false
+  end
+
+  local next_marker = nil
+  for line = marker + 1, last do
+    if is_percent_marker(vim.fn.getline(line)) then
+      next_marker = line
+      break
+    end
+  end
+
+  local start_line = marker + 1
+  local end_line = next_marker and (next_marker - 1) or last
+
+  while start_line <= end_line and vim.fn.getline(start_line):match("^%s*$") do
+    start_line = start_line + 1
+  end
+
+  while end_line >= start_line and vim.fn.getline(end_line):match("^%s*$") do
+    end_line = end_line - 1
+  end
+
+  if start_line > end_line then
+    vim.notify("No code in this # %% cell", vim.log.levels.WARN)
+    return true
+  end
+
+  define_cell(start_line, end_line)
+  return true
+end
+
 function M.define_paragraph_cell()
+  if M.define_percent_cell() then
+    return
+  end
+
   local current = vim.api.nvim_win_get_cursor(0)[1]
   local last = vim.fn.line("$")
   local start_line = current
