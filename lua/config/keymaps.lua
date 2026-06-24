1b900023c41582480e94733944d7c4c0539e5f3e
rasmushr97 <rasmushr97@gmail.com>
2026-06-24 23:11:09 +0200

    Restore native substitute keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Restore Vim's native substitute behavior. LazyVim/flash.nvim maps `s` to
+-- jump/search, but normal Vim uses `s` as `cl` and `S` as `cc`.
+vim.keymap.set("n", "s", "cl", { desc = "Substitute character" })
+vim.keymap.set("n", "S", "cc", { desc = "Substitute line" })
