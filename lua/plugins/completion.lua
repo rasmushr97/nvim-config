c49e367829539887d9542e9c9c0dc5a2fbc51c3c
rasmushr97 <rasmushr97@gmail.com>
2026-06-26 13:26:40 +0200

    Restore delayed completion help

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -6,15 +6,17 @@ return {
         preset = "super-tab",
       },
       completion = {
-        -- Keep completion manual so LSP doesn't query/show suggestions on every keystroke.
-        -- Press <C-Space> when you want the menu.
+        -- Still provide help automatically, but avoid querying/showing instantly on every keystroke.
+        -- Press <C-Space> for immediate completion and <C-k> for signature help.
         menu = {
-          auto_show = false,
+          auto_show = true,
+          auto_show_delay_ms = 250,
         },
         trigger = {
           prefetch_on_insert = false,
-          show_on_keyword = false,
-          show_on_trigger_character = false,
+          show_on_keyword = true,
+          show_on_trigger_character = true,
+          show_on_insert = false,
         },
         documentation = {
           auto_show = false,
@@ -25,8 +27,14 @@ return {
           },
         },
       },
+      sources = {
+        min_keyword_length = 2,
+      },
       signature = {
-        enabled = false,
+        enabled = true,
+        trigger = {
+          enabled = false,
+        },
       },
     },
   },
